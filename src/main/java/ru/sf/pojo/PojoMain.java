96a3d57ca116a994a7a5890bab258bd3413f19de
Sergey Kovalev <dip.deep@mail.ru>
2022-06-09 23:29:40 +0300

    deleted pojo package

--- src/main/java/ru/sf/pojo/PojoMain.java
+++
@@ -1,9 +0,0 @@
-package ru.sf.pojo;
-
-public class PojoMain {
-    public static void main(String[] args) {
-        Pojo pojo = new Pojo.Builder().name("asd").surname("sdf").age(123).build();
-        System.out.println(pojo);
-        Pojo pojo1 = new Pojo.Builder().name("asd").build();
-    }
-}
