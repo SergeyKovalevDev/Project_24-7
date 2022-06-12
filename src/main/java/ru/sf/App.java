d37bde6871e94783e8bf9142489888c76d781bfc
Sergey Kovalev <dip.deep@mail.ru>
2022-06-12 14:58:39 +0300

    added comments in App

--- src/main/java/ru/sf/App.java
+++ src/main/java/ru/sf/App.java
@@ -5,6 +5,7 @@ import ru.sf.enums.UniversityComparatorEnum;
 import ru.sf.models.Student;
 import ru.sf.models.University;
 import ru.sf.parser.XLSXParser;
+import ru.sf.utils.ComparatorSelector;
 
 import java.util.Comparator;
 
@@ -15,6 +16,10 @@ public class App {
 
         Comparator<Student> studentComparator = ComparatorSelector.getStudentComparator(StudentComparatorEnum.BY_FULL_NAME);
         Comparator<University> universityComparator = ComparatorSelector.getUniversityComparator(UniversityComparatorEnum.BY_YEAR_OF_FOUNDATION);
+
+//        Так можно получить экземпляры компараторов без использования "утилитного" класса
+//        Comparator<Student> studentComparator = StudentComparatorEnum.BY_FULL_NAME.getComparatorType();
+//        Comparator<University> universityComparator = UniversityComparatorEnum.BY_YEAR_OF_FOUNDATION.getComparatorType();
 
         xlsxParser.getAllStudentsFromXLSX(filename)
                 .stream()
