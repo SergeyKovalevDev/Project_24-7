91b0b3e81d5ba3722b5163ef898d8e150b98e69d
Sergey Kovalev <dip.deep@mail.ru>
2022-06-16 21:53:44 +0300

    fixed comparator classes

--- src/main/java/ru/sf/utils/ComparatorSelector.java
+++ src/main/java/ru/sf/utils/ComparatorSelector.java
@@ -1,21 +1,33 @@
 package ru.sf.utils;
 
+import ru.sf.comparators.*;
 import ru.sf.enums.StudentComparatorEnum;
 import ru.sf.enums.UniversityComparatorEnum;
-import ru.sf.models.Student;
-import ru.sf.models.University;
-
-import java.util.Comparator;
 
 public class ComparatorSelector {
 
     private ComparatorSelector() {}
 
-    public static Comparator<Student> getStudentComparator(StudentComparatorEnum comparatorEnum) {
-        return comparatorEnum.getComparatorType();
+    public static StudentComparatorInterface getStudentComparator(StudentComparatorEnum comparator) {
+
+        return switch (comparator) {
+
+            case BY_AVG_EXAM_SCORE -> new StudentAvgExamScoreComparator();
+            case BY_CURRENT_COURSE_NUMBER -> new StudentCurrentCourseNumberComparator();
+            case BY_DATE_OF_BIRTH -> new StudentDateOfBirthComparator();
+            case BY_FULL_NAME -> new StudentFullNameComparator();
+            case BY_UNIVERSITY_ID -> new StudentUniversityIdComparator();
+        };
     }
 
-    public static Comparator<University> getUniversityComparator(UniversityComparatorEnum comparatorEnum) {
-        return comparatorEnum.getComparatorType();
+    public static UniversityComparatorInterface getUniversityComparator(UniversityComparatorEnum comparator) {
+        return switch (comparator) {
+            case BY_FULL_NAME -> new UniversityFullNameComparator();
+            case BY_ID -> new UniversityIdComparator();
+            case BY_MAIN_PROFILE -> new UniversityMainProfileComparator();
+            case BY_SHORT_NAME -> new UniversityShortNameComparator();
+            case BY_WEBSITE -> new UniversityWebsiteComparator();
+            case BY_YEAR_OF_FOUNDATION -> new UniversityYearOfFoundationComparator();
+        };
     }
 }
