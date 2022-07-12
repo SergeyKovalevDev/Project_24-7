25a8586636b5a150f3e2c2224385a3d8ac011c72
Sergey Kovalev <dip.deep@mail.ru>
2022-07-12 22:32:40 +0300

    added logging

--- src/main/java/ru/sf/export/JSONExporter.java
+++ src/main/java/ru/sf/export/JSONExporter.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import ru.sf.exceptions.AppException;
 import ru.sf.serializers.LocalDateSerializer;
+import ru.sf.xlsxutils.XLSXParser;
 
 import java.io.FileWriter;
 import java.io.IOException;
@@ -11,22 +12,27 @@ import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDate;
+import java.util.logging.Logger;
 
 public class JSONExporter implements Exportable {
+
+    public static final Logger logger = Logger.getLogger(XLSXParser.class.getName());
+
     @Override
     public void exportToFile(ExportStructure structure, Path filePath) throws AppException {
         try {
             Files.createDirectories(filePath.getParent());
         } catch (IOException ex) {
-            throw new RuntimeException(ex);
+            throw new AppException("Error saving the structure to a file \"" + filePath + "\"");
         }
         try (Writer writer = new FileWriter(String.valueOf(filePath))) {
             GsonBuilder gsonBuilder = new GsonBuilder();
             gsonBuilder.registerTypeAdapter(LocalDate.class, new LocalDateSerializer());
             Gson gson = gsonBuilder.setPrettyPrinting().create();
             gson.toJson(structure, writer);
+            logger.info("The structure has been successfully saved to a file \"" + filePath + "\"");
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new AppException("Error saving the structure to a file \"" + filePath + "\"");
         }
     }
 }
--- src/main/java/ru/sf/export/XMLExporter.java
+++ src/main/java/ru/sf/export/XMLExporter.java
@@ -1,6 +1,7 @@
 package ru.sf.export;
 
 import ru.sf.exceptions.AppException;
+import ru.sf.xlsxutils.XLSXParser;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -10,25 +11,29 @@ import java.io.IOException;
 import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.logging.Logger;
 
 public class XMLExporter implements Exportable {
+
+    public static final Logger logger = Logger.getLogger(XLSXParser.class.getName());
 
     @Override
     public void exportToFile(ExportStructure structure, Path filePath) throws AppException {
         try {
             Files.createDirectories(filePath.getParent());
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new AppException("Error saving the structure to a file \"" + filePath + "\"");
         }
         try (Writer writer = new FileWriter(String.valueOf(filePath))) {
             JAXBContext context = JAXBContext.newInstance(ExportStructure.class);
             Marshaller marshaller = context.createMarshaller();
             marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
             marshaller.marshal(structure, writer);
+            logger.info("The structure has been successfully saved to a file \"" + filePath + "\"");
         } catch (JAXBException e) {
-            throw new AppException("Error JAXB");
+            throw new AppException("Error converting the structure to xml\n" + e);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new AppException("Error saving the structure to a file \"" + filePath + "\"");
         }
     }
 }
